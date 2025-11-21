70911e81db137d454a2e474c8b2414b8c6139abe
Shaan Mirchandani <shaanmi91@gmail.com>
2025-11-20 22:04:20 -0800

    Commit

---
+++ 04-backends/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.7.0"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+    random = {
+        source = "hashicorp/random"
+        version = "~> 3.0"
+    }
+
+    }
+
+     backend "s3" {
+
+     }
+  }
+
+  provider "aws" {
+    region = "us-east-1"
+  }
